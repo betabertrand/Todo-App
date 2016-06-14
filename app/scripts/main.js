c375c659fa6e5a6dad9a52cb1b52744f098c0dec
btsp60 <Bertsp@97>
2016-06-14 17:59:28 -0400

    fixes delete. adds functional checkbox

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,92 +2,67 @@
 
  $(document).ready(function(){
 
-        /*$("#buttonexit").click(function(){
-          $("#itemtodo").remove();
-          });
-		*/
+    
 
 
 
-        var start = '<div class="boxbox2 center outlinecolor"><div class="row" id="itemtodo2"><div class="col-xs-2"><button type="button" id="buttoncircle" class="buttonsize boxcolor no-outline"><i class="fa fa-circle-thin circle"></i></button></div><div class="col-xs-10 hovertext boxcolor box-style3"><div class="col-xs-10" id="todotext"><p class="left text2 textfont" id="todolist">';
+        var start = '<div class="boxbox2 center outlinecolor"><div class="row" id="itemtodo2"><div class="col-xs-2"><button type="button" id="buttoncircle" class="buttonsize boxcolor no-outline buttons"><i class="fa fa-circle-thin circle buttonstyle"></i></button></div><div class="col-xs-10 hovertext boxcolor box-style3"><div class="col-xs-10" id="todotext"><p class="left text2 textfont" id="todolist">';
 
         var finish = '</p></div><div class="col-xs-2"> <button type="button" id="buttonexit" class="update buttonsize no-outline"><i class="material-icons">clear</i></button></div></div></div><div class="row"></div></div></div>';
 
-        var button = '<i id="bc" class="fa fa-dot-circle-o"></i>';
+   
+      var entityMap = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': '&quot;',
+      "'": '&#39;',
+      "/": '&#x2F;'
+      };
 
-       	var buttons = '<i id="buttoncircle" class="fa fa-circle-thin"></i>';
-
-      // $("input").keypress(function(e) {
-        //if(e.which == 13) {
-        //$("#itemtodo").clone().appendTo("#itemtodo2");
-        //}
-        //}); 
+      function escapeHtml(string) {
+      return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+      });
+      }
 
 		
 	
 
 
-       	$("input").keypress(function(e) {
+       	$('input').keypress(function(e) {
 
-       		
+       	var textinput = $('#todoinput').val();
+       	var textlength = $('#todoinput').val().length;
+        var text = escapeHtml(textinput);
 
-
-
-       	var textinput = $("#todoinput").val();
-
-       	var textlength = $("#todoinput").val().length;
-
-
-
+     
       
         if(e.which == 13) {
 
-        	if (textlength >= 3 && $.trim(textinput).length >= 3) {
+        	if ($.trim(text).length >= 3) {
 
-        		 $("#itemtodo").prepend(start + textinput + finish);
+        		 $('#itemtodo').prepend(start + text + finish);
 
-        		$("#todoinput").val("");
+        		$('#todoinput').val('');
 
         	}
 
         	else {
 
-        		alert("Todo input must be >= 3 characters. Not inlcuding spaces.");
+        		alert('Todo input must be >= 3 characters. Not inlcuding spaces.');
         	}
-       
     	}	
     	});
 
+        $('#itemtodo').on('click','#buttonexit', function(e){ //user click on remove text
+        $(this).parents('div').eq(3).remove();
+      })
     
+  
+        $('body').on('click', '.buttonstyle', function() {
+        $(this).toggleClass("fa-circle-thin fa-check-circle-o");
+        });
 
-
-
-       	$("#buttoned").click(function(){
-        $("#buttoncircle").replaceWith(button);
-
-
-        
-
-       	});
-
-       	$("#buttoned").click(function(){
-        $("#bc").replaceWith(buttons);
-       	});
-
-
-
-
-        
-    	$("#itemtodo").on('click','#buttonexit', function(e){ //user click on remove text
-        $("#itemtodo2").remove();
-    	})
-
-
-
-
-
-
-
-     
 
      });
