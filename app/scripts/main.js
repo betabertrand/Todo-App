7adc7fe3094066486560d0d1ba67abe5ed676ac9
btsp60 <Bertsp@97>
2016-06-13 22:15:01 -0400

    adds todos and deletes from top-mustfix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,11 @@
 
  $(document).ready(function(){
 
-        $("#buttonexit").click(function(){
+        /*$("#buttonexit").click(function(){
           $("#itemtodo").remove();
           });
+		*/
+
 
 
         var start = '<div class="boxbox2 center outlinecolor"><div class="row" id="itemtodo2"><div class="col-xs-2"><button type="button" id="buttoncircle" class="buttonsize boxcolor no-outline"><i class="fa fa-circle-thin circle"></i></button></div><div class="col-xs-10 hovertext boxcolor box-style3"><div class="col-xs-10" id="todotext"><p class="left text2 textfont" id="todolist">';
@@ -22,34 +24,70 @@
         //}); 
 
 		
-
+	
 
 
        	$("input").keypress(function(e) {
+
+       		
+
+
 
        	var textinput = $("#todoinput").val();
 
        	var textlength = $("#todoinput").val().length;
 
+
+
       
-        if(e.which == 13 &&  textlength > 3) {
-        $("#itemtodo").append(start + textinput + finish);
+        if(e.which == 13) {
+
+        	if (textlength >= 3 && $.trim(textinput).length >= 3) {
+
+        		 $("#itemtodo").prepend(start + textinput + finish);
+
+        		$("#todoinput").val("");
+
+        	}
+
+        	else {
+
+        		alert("Todo input must be >= 3 characters. Not inlcuding spaces.");
+        	}
+       
     	}	
     	});
 
+    
 
 
 
-       	$("#buttoncircle").click(function(){
+       	$("#buttoned").click(function(){
         $("#buttoncircle").replaceWith(button);
+
+
+        
+
        	});
 
-       	$("#bc").click(function(){
+       	$("#buttoned").click(function(){
         $("#bc").replaceWith(buttons);
        	});
 
-       	fun
 
 
+
+        
+    	$("#itemtodo").on('click','#buttonexit', function(e){ //user click on remove text
+        $("#itemtodo2").remove();
+    	})
+
+
+
+
+
+
+
+     
 
      });
