a9770994017ebad59ba8922f69b95d84f1df42d9
btsp60 <Bertsp@97>
2016-06-14 23:01:54 -0400

    adds semifunctional todo counter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,11 +6,17 @@
 
 
 
-        var start = '<div class="boxbox2 center outlinecolor"><div class="row" id="itemtodo2"><div class="col-xs-2"><button type="button" id="buttoncircle" class="buttonsize boxcolor no-outline buttons"><i class="fa fa-circle-thin circle buttonstyle"></i></button></div><div class="col-xs-10 hovertext boxcolor box-style3"><div class="col-xs-10" id="todotext"><p class="left text2 textfont" id="todolist">';
+        var start = '<div class="boxbox2 center outlinecolor"><div class="row" id="itemtodo2"><div class="col-xs-2"><button type="button" id="buttoncircle" class="buttonsize boxcolor no-outline buttons"><i class="fa fa-circle-thin circle buttonstyle"></i></button></div><div class="col-xs-10 hovertext boxcolor box-style3"><div class="col-xs-10" id="todotext"><p class="left text2 textfont todude" id="todolist">';
 
         var finish = '</p></div><div class="col-xs-2"> <button type="button" id="buttonexit" class="update buttonsize no-outline"><i class="material-icons">clear</i></button></div></div></div><div class="row"></div></div></div>';
 
-   
+        var itemsleftstart = '<p class="text3 textfont itemsleft">';
+        var itemsleftfinish = ' items left</p>'
+
+        var x = 0;
+
+
+
       var entityMap = {   //keeping texts in strings
       "&": "&amp;",
       "<": "&lt;",
@@ -46,6 +52,9 @@
 
         		$('#todoinput').val('');
 
+            x++;
+            $(".itemsleft").html(itemsleftstart+x+itemsleftfinish);
+
         	}
 
         	else {
@@ -54,15 +63,48 @@
         	}
     	}	
     	});
+        
+
+
 
         $('#itemtodo').on('click','#buttonexit', function(e){ //user click on remove text
           $(this).parents('div').eq(3).remove();
+          x--;
+          $(".itemsleft").html(itemsleftstart+x+itemsleftfinish);
         })
     
   
         $('body').on('click', '.buttonstyle', function() {   //checks boxes
           $(this).toggleClass("fa-circle-thin fa-check-circle-o");
+
+        
+          //x--;
+          //$(".itemsleft").html(itemsleftstart+x+itemsleftfinish);
+          
+          //$(".todude").toggleClass("crossword");
+         
         });
+
+        $('body').on('click', '#downbutton', function() {   //checks all boxes. needs if to work
+
+          //if ((".buttonstyle").hasClass(".fa-circle-thin")) {
+
+            $(".buttonstyle").toggleClass("fa-check-circle-o fa-circle-thin");
+          //}
+
+          
+
+     
+
+          //$(".todude").toggleClass("crossword");
+         
+        });
+
+
+
+       
+        
+
 
 
         $("body").on("click", "#active", function() {        //hides checked boxes
@@ -90,6 +132,22 @@
         });
 
 
+/*
+        $("#all").click(function(){
+
+          $.toggle(
+
+          function() {        //shows all boxes
+          $(".fa-circle-thin").parent().parent().parent().show();
+        },
+
+        function() {        //shows all boxes
+          $(".fa-check-circle-o").parent().parent().parent().show();
+
+        })
+        });
+
+*/
 
 
        
